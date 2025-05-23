62f53363c6e35c864630f739dd270db1cba6f564
Tyler Anderson <tsm.anderson@gmail.com>
2025-05-23 06:35:20 -0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'field-report-v7'; // 🔁 bump this for every update
+const CACHE_NAME = 'field-report-v7.1'; // 🔁 bump this for every update
 
 const urlsToCache = [
   'index.html',
