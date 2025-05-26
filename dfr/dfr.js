cea00f7b1ae8ae9f1c2f00a875448fdbf14ca480
Thayer <tsm.anderson@gmail.com>
2025-05-26 07:45:42 -0700

    v8.7

--- dfr/dfr.js
+++ dfr/dfr.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'field-report-v4'; // 🔁 bump this for every update
+const CACHE_NAME = 'field-report-v8.7'; // 🔁 bump this for every update
 
 let listData = {};
 
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'field-report-v8.6'; // 🔁 bump this for every update
+const CACHE_NAME = 'field-report-v8.7'; // 🔁 bump this for every update
 
 const urlsToCache = [
   './',
