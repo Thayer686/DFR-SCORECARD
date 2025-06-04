c479a612b926f9f853cff7d84a4f89abdae578b9
Thayer <tsm.anderson@gmail.com>
2025-06-04 11:09:09 -0700

    code updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'field-report-v1.0.2'; // 🔁 bump this for every update
+const CACHE_NAME = 'field-report-v1.0.3'; // 🔁 bump this for every update
 
 const urlsToCache = [
   './',
